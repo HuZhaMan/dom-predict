922f337ba91c7f7c3fa69849c1c76040725c990f
bluesli <1807103336@qq.com>
2019-11-22 15:15:43 +0800

    Update mail_conf.py

--- saninco_docs/mail_conf.py
+++ saninco_docs/mail_conf.py
@@ -1,6 +1,6 @@
 mail_host = "smtp.163.com"
-mail_user = "399018773@163.com"
-mail_pass = "saninco123"
-sender = '399018773@163.com'
-from_name = 'Realtor Data AI'
-receivers = ['399018773@qq.com', 'huibin.guan@xinketechnology.com', 'yuhang.xing@xinketechnology.com']
+mail_user = 
+mail_pass = 
+sender = 
+from_name = 
+receivers = 
